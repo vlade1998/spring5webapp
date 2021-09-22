88f73737edcd7efb8c137cf811f847cea07d52d2
vlade1998 <vladejrs@hotmail.com>
2021-09-21 22:27:07 -0300

    fix: changing long to Long

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -16,7 +16,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     private String title;
     private String isbn;
