849ae1d86a5b176a5ed6fa5a719934ccf802c89d
vlade1998 <vladejrs@hotmail.com>
2021-09-21 22:00:19 -0300

    feat: initial POJOs

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -0,0 +1,46 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.Set;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private Set<Author> authors;
+
+
+    public Book() {
+    }
+
+
+
+    public Book(String title, String isbn, Set<Author> authors) {
+        this.title = title;
+        this.isbn = isbn;
+        this.authors = authors;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+}
